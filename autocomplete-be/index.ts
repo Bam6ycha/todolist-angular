5768f78036637f7356078a706822d1c35e657754
Dmitrii Kosarev <kosarev-d@mail.ru>
2022-01-25 23:51:08 +0300

    feat: read cities.json using readblestream. refactor: some codestyle changes

--- autocomplete-be/index.ts
+++ autocomplete-be/index.ts
@@ -1,50 +1,50 @@
-import http = require('http');
+import http from 'http';
 import { createAutoComplete } from '../auto-complete/index';
-import { data } from './utilities/cities';
 import {
   getQueryFromUrl,
   generateEtag,
-  getStats,
   getFileInfo,
   isEtagChanged,
+  readFile,
+  sendCachedData,
+  sendData,
+  setCache,
+  sendUserError,
+  sendServerError,
 } from './utilities/utilities';
 
-const autoComplete = createAutoComplete(data) as (prefix: string) => string[];
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(async (req, res) => {
-  const stats = await getStats();
-  const eTag = generateEtag(stats);
-  const lastModified = await getFileInfo();
+  try {
+    const data = await readFile();
+    const autoComplete = createAutoComplete(data) as (
+      prefix: string,
+    ) => string[];
 
-  if (isEtagChanged(eTag, req.headers)) {
-    res.setHeader('last-modified', `${lastModified}`);
-    res.statusMessage = 'Not Modified';
-    res.statusCode = 304;
-    res.end();
-    return;
-  }
+    const url = req.url as string;
+    const query = getQueryFromUrl(url);
+    const results = autoComplete(query);
+    const method = req.method;
 
-  const url = req.url as string;
-  const query = getQueryFromUrl(url);
-  const results = autoComplete(query);
-  const method = req.method;
+    const eTag = generateEtag(results.join(' '));
 
-  if (results.length && method === 'GET') {
-    res.statusCode = 200;
-    res.statusMessage = 'OK';
-    res.setHeader('access-control-allow-origin', `*`);
-    res.setHeader('Content-type', 'application/json');
-    res.setHeader('etag', `${eTag}`);
-    res.setHeader('Cache-Control', 'max-age=3600');
-    res.setHeader('last-modified', `${lastModified}`);
-    res.write(JSON.stringify(results));
-    res.end();
-  } else {
-    res.statusCode = 404;
-    res.statusMessage = 'Not Found';
-    res.write(`${res.statusCode} ${res.statusMessage}`);
-    res.end();
+    const lastModified = await getFileInfo();
+
+    if (isEtagChanged(eTag, req.headers)) {
+      sendCachedData(res, lastModified);
+      return;
+    }
+
+    if (results.length && method === 'GET') {
+      setCache(res, eTag, lastModified);
+      sendData(res, results);
+      console.log(12);
+    } else {
+      sendUserError(res);
+    }
+  } catch (error) {
+    sendServerError(res);
   }
 });
 
--- autocomplete-be/utilities/utilities.ts
+++ autocomplete-be/utilities/utilities.ts
@@ -1,11 +1,11 @@
 import path from 'path';
 import fs from 'fs/promises';
-import eTag = require('etag');
-import { IncomingHttpHeaders } from 'http';
+import eTag from 'etag';
+import { IncomingHttpHeaders, ServerResponse } from 'http';
 
-const PATH_TO_DATA = path.join(__dirname, 'cities.ts');
+const PATH_TO_DATA = path.join(__dirname, 'cities.json');
 
-export const getQueryFromUrl = (url: string): string => {
+const getQueryFromUrl = (url: string): string => {
   if (isMatch(url)) {
     return url.slice(url.indexOf('=') + 1);
   }
@@ -15,31 +15,93 @@ export const getQueryFromUrl = (url: string): string => {
 
 const isMatch = (url: string): boolean => {
   const validateURL = /complete=.*/;
+
   if (!!url.match(validateURL)) {
     return true;
   }
+
   return false;
 };
 
-export const getFileInfo = async () => {
+const getFileInfo = async () => {
   const stats = await fs.stat(PATH_TO_DATA);
   return stats.mtime.toUTCString();
 };
 
-export const generateEtag = (stat: eTag.StatsLike) => {
-  return eTag(stat);
+const generateEtag = (body: string) => {
+  return eTag(body);
 };
 
-export const getStats = async () => {
-  const { ctime, mtime, ino, size } = await fs.stat(PATH_TO_DATA);
-
-  return { ctime, mtime, ino, size };
-};
-
-export const isEtagChanged = (
+const isEtagChanged = (
   etag: string,
   requestHeaders: IncomingHttpHeaders,
 ): boolean => {
   const noneMatch = requestHeaders['if-none-match'];
   return etag === noneMatch;
+};
+
+const readFile = async (): Promise<string[] | Error> => {
+  const fileHandle = await fs.open(PATH_TO_DATA, 'r');
+  const readStream = fileHandle.createReadStream();
+
+  return new Promise((resolve, reject) => {
+    let data = '';
+    readStream.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    readStream.on('error', () => reject(new Error('Something went wrong')));
+
+    readStream.on('end', () => resolve(JSON.parse(data)));
+  });
+};
+
+const sendCachedData = (res: ServerResponse, lastModified: string): void => {
+  res.setHeader('last-modified', `${lastModified}`);
+  res.statusMessage = 'Not Modified';
+  res.statusCode = 304;
+  res.end();
+  return;
+};
+
+const sendData = (res: ServerResponse, data: string[]) => {
+  res.statusCode = 200;
+  res.statusMessage = 'OK';
+  res.setHeader('access-control-allow-origin', `*`);
+  res.setHeader('Content-type', 'application/json');
+  res.write(JSON.stringify(data));
+  res.end();
+};
+
+const setCache = (res: ServerResponse, eTag: string, lastModified: string) => {
+  res.setHeader('etag', `${eTag}`);
+  res.setHeader('Cache-Control', 'max-age=3600');
+  res.setHeader('last-modified', `${lastModified}`);
+};
+
+const sendUserError = (res: ServerResponse) => {
+  res.statusCode = 404;
+  res.statusMessage = 'Not Found';
+  res.write(`${res.statusCode} ${res.statusMessage}`);
+  res.end();
+};
+
+const sendServerError = (res: ServerResponse) => {
+  res.statusCode = 500;
+  res.statusMessage = `Something went wrong`;
+  res.write(`${res.statusCode} ${res.statusMessage}`);
+  res.end();
+};
+
+export {
+  getQueryFromUrl,
+  generateEtag,
+  getFileInfo,
+  isEtagChanged,
+  readFile,
+  sendCachedData,
+  sendData,
+  setCache,
+  sendUserError,
+  sendServerError,
 };
